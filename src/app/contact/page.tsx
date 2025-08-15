b4b3c718c6b026e9010f652055bab3955f2d77d5
unknown <ph.hoangloc@gmail.com>
2025-08-15 17:00:27 +0900

    0815

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,38 @@
 'use client'
+import axios from 'axios'
 import React, { useState } from 'react'
-
+import { useRouter } from 'next/navigation'
 const Page = () => {
     const [_name, set_name] = useState<string>("")
     const [_email, set_email] = useState<string>("")
     const [_content, set_content] = useState<string>("")
+    const [_refresh, set_refresh] = useState<number>(0)
+    const [_warn, set_warn] = useState<string>("")
+
+    const toPage = useRouter()
+    const sendMail = async (body: { name: string, email: string, content: string }) => {
+        const newBody: {
+            receipt?: string,
+            name: string,
+            email: string,
+            content: string
+        } = body
+        newBody.receipt = "h-loc@astem-co.co.jp,kimura@com-sagano.com"
+
+        const result = await axios.post(process.env.api_url + "api/mail", newBody)
+        if (result.data.success) {
+            set_warn(result.data.data)
+            setTimeout(() => {
+                set_warn("")
+                set_name("")
+                set_email("")
+                set_content("")
+                set_refresh(n => n + 1)
+            }, 3000)
+        }
+    }
     return (
-        <div>
+        <div className='bg-sky-500'>
             <div className="  p-10 max-w-(--xl) m-auto">
                 <div className="text-white">
                     <h2 className='text-2xl font-bold'>Contact</h2>
@@ -15,7 +41,7 @@ const Page = () => {
                         <h1 className='text-3xl border-l-4 pl-2 font-semibold tracking-[1px] '>お問い合わせ</h1>
                     </div>
                 </div>
-                <div className="contact grid max-w-(--sm) mx-auto bg-white py-10 p-4 md:p-8 lg:p-12 rounded shadow">
+                <div className="contact grid max-w-(--sm) mx-auto bg-white py-10 p-4 md:p-8 lg:p-12 rounded shadow" key={_refresh}>
                     <h1 className="font-bold text-2xl text-center text-titleTXT">お問い合わせ</h1>
                     <div>名称</div>
                     <input className="h-12 border rounded border-slate-300" onChange={(e) => set_name(e.currentTarget.value)} value={_name} />
@@ -25,7 +51,8 @@ const Page = () => {
                     <div className="h-4"></div>
                     <div>内容</div>
                     <textarea className="h-36 border rounded border-slate-300" value={_content} onChange={(e) => set_content(e.currentTarget.value)} />
-                    <button className="bg-search-bg w-max mx-auto mt-4 text-lg px-4 py-2 rounded text-white" onClick={() => console.log("お問い合わせは送信されました。応答が届きましたら、ボイスメールをご確認ください。")} >送信</button>
+                    <button className="bg-sky-600 w-max mx-auto mt-4 text-lg px-4 py-2 rounded text-white" onClick={() => sendMail({ name: _name, email: _email, content: _content })} >送信</button>
+                    <div className='my-2 text-red-500 text-center'>{_warn}</div>
                 </div>
             </div>
         </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,22 +8,22 @@ const Footer = () => {
         <div className='bg-white p-10 text-blueTXT'>
             <div className="md:flex max-w-(--lg) m-auto justify-between">
                 <div className='text-center mb-4'>
-                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/home#j")}>業界を知る</h4>
-                    <p onClick={() => toPage.push("/home#f")} className='cursor-pointer hover:text-sky-600 hover:font-semibold'>施設紹介</p>
-                    <p onClick={() => toPage.push("/home#h")} className='cursor-pointer hover:text-sky-600 hover:font-semibold'>先輩たちの声</p>
+                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("#j")}>業界を知る</h4>
+                    <p onClick={() => toPage.push("#f")} className='cursor-pointer hover:text-sky-600 hover:font-semibold'>施設紹介</p>
+                    <p onClick={() => toPage.push("#h")} className='cursor-pointer hover:text-sky-600 hover:font-semibold'>先輩たちの声</p>
                 </div>
                 <div className='text-center mb-4'>
-                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/home/search/n/a/b/s/c")}>仕事を探す</h4>
+                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/search")}>仕事を探す</h4>
 
                 </div>
                 <div className='text-center mb-4'>
-                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/home/facility")}>施設一覧</h4>
+                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/facility")}>施設一覧</h4>
                 </div>
                 <div className='text-center mb-4'>
-                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/home/news")}>ニュース</h4>
+                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/news")}>ニュース</h4>
                 </div>
                 <div className='text-center mb-4'>
-                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/home/contact")}>お問い合わせ</h4>
+                    <h4 className='font-bold text-xl cursor-pointer hover:text-sky-600' onClick={() => toPage.push("/contact")}>お問い合わせ</h4>
                 </div>
             </div>
             <div className="fixed w-[125px] aspect-square bottom-1 right-1 rounded-[50%] backdrop-brightness-75 flex flex-col justify-center text-center font-bold text-white text-xl">
